919268ea1539cae7298298997a65c3bf8b20a76e
Hyeonsik <hyeonsik98@naver.com>
2020-12-08 18:26:57 +0000

    rest folders 201209 from dl_pain

---
+++ Outcome/Table 1/.ipynb_checkpoints/table1-checkpoint.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import scipy.stats as stat
+import tableone
+import datetime
+
+# +
+df = pd.read_csv('T1_contents_final.csv')
+df['event']=df['Value']>4
+df['event']=df['event'].astype(int)
+df['gender']=df['gender'].astype(str)
+
+print(df.dtypes)
+# -
+
+# 그룹 지정 컬럼명
+group_col = 'event'
+
+# 통계 돌릴 컬럼 목록
+#cols = df.columns.tolist()  # 전체 컬럼을 모두 지정
+#cols.remove(group_col)
+cols = ['age', 'gender', 'weight','height', 'Anetime','Optime']  # 내가 원하는 변수만 출력 순서를 지정
+
+# 카테고리 변수인 컬럼을 찾아냄
+categorical_cols = ['gender']  # 카테고리 변수 수동 지정
+continuous_cols = ['age', 'weight','height', 'Anetime','Optime']  # 연속 변수 수동 지정
+for c in cols:  # 카테고리 변수인지 자동으로 확인
+    if c in categorical_cols:
+        continue
+    if c in continuous_cols:  # 사용자가 수기로 지정한 경우
+        try:
+            df[c] = df[c].astype(float)  # 숫자 형태로 변환해보고 에러나면 사용자에게 알림
+        except:
+            print('variable {} contains non-numeric value'.format(c))
+            categorical_cols.append(c)
+            continue
+        continue
+    try:
+        df[c] = df[c].astype(float)  # 숫자 형태로 변환해보고 에러나면 카테고리 변수
+    except:
+        categorical_cols.append(c)
+        continue
+    # 숫자 형태인 경우라도
+    if len(df[c].unique()) < 6: # 유일 값이 6개 미만이면 카테고리 변수
+        categorical_cols.append(c)
+for c in categorical_cols: # 카테고리 변수는 전부 문자열로 바꿈
+    df[c] = df[c].astype(str)
+
+# 비정규 분포인 컬럼을 찾아냄
+non_normal_cols = []  # 전부 정규분포 --> 모수 통계
+for c in cols:  # Shapiro-Wilk test 를 이용하여 각 변수가 정규 분포인지 확인
+    if c in categorical_cols:
+        continue
+    if stat.shapiro(df[c])[1] < 0.05:
+        non_normal_cols.append(c)
+
+# 군간 비교 수행 
+df_res = tableone.TableOne(df, columns=cols, categorical=categorical_cols, nonnormal=non_normal_cols, groupby=group_col, pval=True, htest_name=True)
+
+print(non_normal_cols)
+
+# 결과 저장
+df_res.to_csv('result_final.csv')
---
+++ Outcome/delong.py
@@ -0,0 +1,47 @@
+def roc_ci(y_true, y_pred):
+    total = len(y_true)
+    success = roc_auc_score(y_true, y_pred) * total
+    alpha = 0.05
+    lower = stat.beta.ppf(alpha / 2, success, total - success + 1)
+    upper = stat.beta.ppf(1 - alpha / 2, success + 1, total - success)
+    return lower, upper
+
+def auc2(X, Y):
+    return 1/(len(X)*len(Y)) * sum([kernel(x, y) for x in X for y in Y])
+
+def kernel(X, Y):
+    return .5 if Y==X else int(Y < X)
+
+def structural_components(X, Y):
+    V10 = [1/len(Y) * sum([kernel(x, y) for y in Y]) for x in X]
+    V01 = [1/len(X) * sum([kernel(x, y) for x in X]) for y in Y]
+    return V10, V01
+
+def get_S_entry(V_A, V_B, auc_A, auc_B):
+    return 1/(len(V_A)-1) * sum([(a-auc_A)*(b-auc_B) for a,b in zip(V_A, V_B)])
+
+def z_score(var_A, var_B, covar_AB, auc_A, auc_B):
+    return (auc_A - auc_B)/((var_A + var_B - 2*covar_AB)**(.5))
+
+def group_preds_by_label(preds, actual):
+    X = [p for (p, a) in zip(preds, actual) if a]
+    Y = [p for (p, a) in zip(preds, actual) if not a]
+    return X, Y
+
+def delong_roc_test(actual, preds_A, preds_B):
+    X_A, Y_A = group_preds_by_label(preds_A, actual)
+    X_B, Y_B = group_preds_by_label(preds_B, actual)
+    V_A10, V_A01 = structural_components(X_A, Y_A)
+    V_B10, V_B01 = structural_components(X_B, Y_B)
+    auc_A = auc2(X_A, Y_A)
+    auc_B = auc2(X_B, Y_B)
+    # Compute entries of covariance matrix S (covar_AB = covar_BA)
+    var_A = (get_S_entry(V_A10, V_A10, auc_A, auc_A) * 1/len(V_A10)
+            + get_S_entry(V_A01, V_A01, auc_A, auc_A) * 1/len(V_A01))
+    var_B = (get_S_entry(V_B10, V_B10, auc_B, auc_B) * 1/len(V_B10)
+            + get_S_entry(V_B01, V_B01, auc_B, auc_B) * 1/len(V_B01))
+    covar_AB = (get_S_entry(V_A10, V_B10, auc_A, auc_B) * 1/len(V_A10)
+                + get_S_entry(V_A01, V_B01, auc_A, auc_B) * 1/len(V_A01))
+    # Two tailed test
+    z = z_score(var_A, var_B, covar_AB, auc_A, auc_B)
+    return stat.norm.sf(abs(z))*2
